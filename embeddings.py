f963f3c39240971dd3dc51cda446eea5dacc1984
bhoominelwade <nelwadebhoomi@gmail.com>
2026-06-27 20:58:43 +0530

    text/paragraph embedding

--- embeddings.py
+++ embeddings.py
@@ -11,8 +11,12 @@ import numpy as np #make a function to calculate cosine similarity search
 
 model = SentenceTransformer('all-MiniLM-L6-v2')
 
-sentance = ["Today the weather is hot","its not raining in mumbai","I like winters"]
-sentance_embedding = model.encode(sentance)
+# sentance = ["Today the weather is hot","its not raining in mumbai","I like winters"]
+# sentance_embedding = model.encode(sentance)
+
+text = "weather today is hot. It is not raining in Mumbai . I like winters over summers and rain.I love to go outside. It is a cool day in Mumbai"
+chunks = text.split(".")
+chunks_embedding = model.encode(chunks)
 
 query = "How is the weather in mumbai"
 query_embedding = model.encode(query)
@@ -20,9 +24,9 @@ query_embedding = model.encode(query)
 def cosine(a,b): #function to calculate cosine similarity
     return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
 
-for i in range(len(sentance_embedding)):
-    score = cosine(sentance_embedding[i], query_embedding)
-    print(f"{sentance[i]} => score {score}")
+for i in range(len(chunks_embedding)):
+    score = cosine(chunks_embedding[i], query_embedding)
+    print(f"{chunks[i]} => score {score}")
     
 #for one vectore comparison   
 # print(cosine(sentace_embedding, query_embedding)) 
